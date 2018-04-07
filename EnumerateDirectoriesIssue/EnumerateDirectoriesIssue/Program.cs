4aebbfca8010acbb06553e0d71f6330230022664
Ashish Jain (NUGET) <asja@microsoft.com>
2018-04-06 17:13:54 -0700

    Simple dotnet app to enumerate directory in rhel72

---
+++ EnumerateDirectoriesIssue/EnumerateDirectoriesIssue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace EnumerateDirectoriesIssue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Please pass a dirextory path");
+                return;
+            }
+
+            var dir = args[0];
+
+            Console.WriteLine("Dir Name : " + dir);
+
+            Console.WriteLine("Directory exists : " + Directory.Exists(dir));
+
+            if (Directory.Exists(dir))
+            {
+                Console.WriteLine("Below is the list of child directories found in this directory:");
+
+                foreach (var subDir in Directory.EnumerateDirectories(dir))
+                {
+                    Console.WriteLine(subDir);
+                }
+            }
+        }
+    }
+}
